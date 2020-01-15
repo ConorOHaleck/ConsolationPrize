afc9844e080a0ff9b43b21c44c1a9905ba53533f
Conor O'Haleck <conorohaleck@gmail.com>
2020-01-15 14:18:10 -0800

    Added documentation to Course properties

--- ConsolationPrize/Course.cs
+++ ConsolationPrize/Course.cs
@@ -8,9 +8,21 @@ namespace ConsolationPrize
 {
     class Course
     {
+        /// <summary>
+        /// Title of the course, as it appears in the course class schedule
+        /// </summary>
         public string CourseTitle { get; set; }
+        /// <summary>
+        /// Text description of the course (as appears in the course class schedule)
+        /// </summary>
         public string Description { get; set; }
+        /// <summary>
+        /// The unique course ID assigned to each course
+        /// </summary>
         public int CourseID { get; set; }
+        /// <summary>
+        /// How many credits earned upon completion of the course
+        /// </summary>
         public byte Credits { get; set; }
 
     }
