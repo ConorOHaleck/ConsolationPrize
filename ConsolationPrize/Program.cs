fe56b0c9ff0a6e7c3d9cd3b8c94f71216807054c
Conor O'Haleck <conorohaleck@gmail.com>
2020-01-15 13:20:49 -0800

    Add project files.

---
+++ ConsolationPrize/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolationPrize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
